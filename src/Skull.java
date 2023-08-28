0b36d9b027eff2a3c51e79dd0b735421feaf8df0
Adam Vadala-Roth <fenderstratman@gmail.com>
2023-08-28 17:19:00 -0400

    Errors Corrected
    Corrected capitalization errors and library import errors still needs more testing

--- src/Skull.java
+++ src/Skull.java
@@ -13,61 +13,61 @@ public class BowlerStudioCAD {
     }
 
     public static CSG servo() {
-        return union(
-            cylinder(6, 6).transformed(translate(0, 0, 40)),
-            union(
-                cube(20, 42, 40),
-                cube(20, 55, 3).transformed(translate(0, -6, 30))
+        return CSG.unionAll(
+            Cylinder.cylinder(6, 6).transformed(translate(0, 0, 40)),
+            CSG.unionAll(
+                Cube.cube(20, 42, 40),
+                Cube.cube(20, 55, 3).transformed(translate(0, -6, 30))
             ).transformed(translate(-10, -10, 0)),
-            cylinder(6, 100).transformed(translate(0, -11, 0)),
-            cylinder(9, 100).transformed(translate(0, -25, 0)),
-            cylinder(4, 100).transformed(translate(-5, -13, 0)),
-            cylinder(4, 100).transformed(translate(5, -13, 0)),
-            cylinder(4, 100).transformed(translate(-5, -13 + 48, 0)),
-            cylinder(4, 100).transformed(translate(5, -13 + 48, 0))
+            Cylinder.cylinder(6, 100).transformed(translate(0, -11, 0)),
+            Cylinder.cylinder(9, 100).transformed(translate(0, -25, 0)),
+            Cylinder.cylinder(4, 100).transformed(translate(-5, -13, 0)),
+            Cylinder.cylinder(4, 100).transformed(translate(5, -13, 0)),
+            Cylinder.cylinder(4, 100).transformed(translate(-5, -13 + 48, 0)),
+            Cylinder.cylinder(4, 100).transformed(translate(5, -13 + 48, 0))
         );
     }
 
     public static CSG mouthHole() {
         return hull(
-            cylinder(200, 5).transformed(translate(40 - 10, 10 - 10, -50).rotated(Vector3.UNIT_Z, 10).rotated(Vector3.UNIT_X, -5)),
-            cylinder(200, 5).transformed(translate(40 - 10, 15 - 10, -50).rotated(Vector3.UNIT_Z, 10))
+            Cylinder.cylinder(200, 5).transformed(translate(40 - 10, 10 - 10, -50).rotated(Vector3.UNIT_Z, 10).rotated(Vector3.UNIT_X, -5)),
+            Cylinder.cylinder(200, 5).transformed(translate(40 - 10, 15 - 10, -50).rotated(Vector3.UNIT_Z, 10))
         );
     }
 
     public static CSG mouthServoMount() {
-        return union(
-            cube(1, 10, 10).transformed(rotate(90, Vector3.UNIT_X).translate(50, -14 + 5, 35)),
-            cube(1, 10, 10).transformed(rotate(90, Vector3.UNIT_X).translate(50, 10 + 5, 35))
+        return CSG.unionAll(
+            Cube.cube(1, 10, 10).transformed(rotate(90, Vector3.UNIT_X).translate(50, -14 + 5, 35)),
+            Cube.cube(1, 10, 10).transformed(rotate(90, Vector3.UNIT_X).translate(50, 10 + 5, 35))
         ).transformed(rotate(-15, Vector3.UNIT_Z));
     }
 
     public static CSG teensyMount() {
-        return union(
-            cylinder(20, 3).transformed(translate(-47, -5, 30)),
-            cylinder(20, 3).transformed(translate(-47 + 25, -5, 30)),
-            cylinder(20, 3).transformed(translate(-47, -5 + 65, 30)),
-            cylinder(20, 3).transformed(translate(-47 + 25, -5 + 65, 30))
+        return CSG.unionAll(
+            Cylinder.cylinder(20, 3).transformed(translate(-47, -5, 30)),
+            Cylinder.cylinder(20, 3).transformed(translate(-47 + 25, -5, 30)),
+            Cylinder.cylinder(20, 3).transformed(translate(-47, -5 + 65, 30)),
+            Cylinder.cylinder(20, 3).transformed(translate(-47 + 25, -5 + 65, 30))
         );
     }
 
     public static CSG skull() {
-        CSG skullTop = union(
+        CSG skullTop = CSG.unionAll(
             eyesocket().transformed(translate(-25, -52, 64).rotated(Vector3.UNIT_Y, 270)),
             eyesocket().transformed(translate(-25 + 53, -52, 64).rotated(Vector3.UNIT_Y, 270)),
             difference(
                 CSG.fromSTL("skull_bottom_cut.stl").transformed(scale(1.7, 1.7, 1.7).rotated(Vector3.UNIT_Z, 43)),
-                cylinder(40, 55).transformed(translate(0, 0, -26 + 25))
+                Cylinder.cylinder(40, 55).transformed(translate(0, 0, -26 + 25))
             ).transformed(translate(0, 10, 85))
         );
 
-        CSG skullBottom = union(
+        CSG skullBottom = CSG.unionAll(
             eyesocket().transformed(translate(-25, -52, 64).rotated(Vector3.UNIT_Y, 270)),
             eyesocket().transformed(translate(-25 + 53, -52, 64).rotated(Vector3.UNIT_Y, 270)),
             servo().transformed(translate(0, 0, 5)),
             hull(
-                cylinder(20, 10).transformed(translate(-7, 65, 30)),
-                cylinder(20, 10).transformed(translate(7, 65, 30))
+                Cylinder.cylinder(20, 10).transformed(translate(-7, 65, 30)),
+                Cylinder.cylinder(20, 10).transformed(translate(7, 65, 30))
             ),
             teensyMount(),
             mouthHole(),
@@ -75,7 +75,7 @@ public class BowlerStudioCAD {
         );
 
         return difference(
-            union(skullTop, skullBottom),
+            CSG.unionAll(skullTop, skullBottom),
             skullBottom
         );
     }
